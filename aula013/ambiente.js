0efa87b0099a3fcf84b1046865a029c356654550
Lucas Pereira <71991303+Lucas-Souza-Dev@users.noreply.github.com>
2022-12-27 22:23:47 -0300

    aula13 repetiçoes

---
+++ aula013/ambiente.js
@@ -0,0 +1,15 @@
+//Estruturas de repetiçao
+
+//While - Enquanto
+var a = 1
+    while(a<=10){
+        console.log(`${a}° vez`)
+        a++
+    }
+
+// do While - Faça, enquanto
+var b = 1
+    do{
+        console.log(`${b}° Lugar`)
+        b++
+    }while(b <= 5)
