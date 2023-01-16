e7bb7cdb14aabbe6db42ee8e6321b3c6fbe7072d
Lucas Pereira <71991303+Lucas-Souza-Dev@users.noreply.github.com>
2023-01-16 20:06:29 -0300

    terminado exercício 17

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,10 +1,19 @@
 function tabuada(){
-    let n = document.getElementById('txtn')
+    let num = document.getElementById('txtn')
     let tab = document.getElementById('seltab')
 
-    if(n.ariaValueMax.length == 0){
+    if(num.value.length == 0){
         window.alert('[ERRO] Por favor, digite um número!')
     }else{
         let n = Number(num.value)
+        let c = 1
+        tab.innerHTML=''
+        while (c <= 10) {
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
     }
 }
