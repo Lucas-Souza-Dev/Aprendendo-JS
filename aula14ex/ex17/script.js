dc9749238565e4eb92fb3f2495f0be59cabb85b3
Lucas Pereira <71991303+Lucas-Souza-Dev@users.noreply.github.com>
2023-01-16 18:41:48 -0300

    pt1

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -1,1 +1,10 @@
-alert('Oi essa é a pagina modelo')
+function tabuada(){
+    let n = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if(n.ariaValueMax.length == 0){
+        window.alert('[ERRO] Por favor, digite um número!')
+    }else{
+        let n = Number(num.value)
+    }
+}
